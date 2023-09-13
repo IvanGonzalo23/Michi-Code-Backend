9d0c4c55b06f9c58a891d40b8af00e6373d1d7af
Ivan Gonzalo <alzabetivan@gmail.com>
2023-09-13 00:37:35 -0300

    Se actualizo los endpoints /inicio y /createuser para poder contener los id, hay un bug en donde cuando se cree el usuario el id estara guardado y mostrado directamente en /inicio pero el nombre de usuario o email no, se mostrara del usuario anterior,

--- Api/__init__.py
+++ Api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask,jsonify,render_template,request,redirect,url_for,session
 from config import Config
 from .models.Users import User
 from .routes.auth_bp import AuthBlueprint
+from .models.Servers import Server
 
 
 def init_app():
@@ -17,10 +18,10 @@ def init_app():
     def login():
         if request.method == 'POST':
                 email_username = request.form.get('email_username')
-
                 contrasenia = request.form.get('contrasenia')
-                
-                if User.verificar_credenciales(email_username, contrasenia):
+                verificado, user_id = User.verificar_credenciales(email_username, contrasenia)
+                if verificado:
+                    session["user_id"] = user_id
                     session['email_username'] = email_username
                     session["contrasenia"] = contrasenia
                     return redirect(url_for('muro'))
@@ -30,16 +31,56 @@ def init_app():
     @app.route("/inicio", methods=["GET","POST"])
     def muro():
         
-        email_username = session.get('email_username')
-    
-        if email_username:
-            print(f"Usuario/email: {email_username}")
-            return render_template('inicio.html')
-        else:
-            return redirect('/login')
+        """
+        Despues del logeo recibe los datos del usuario, pero si crea un usuario y entra
+        recibira el ID del usuario recien creado pero no el nombre o correo, en este caso
+        recibira el nombre o correo del usuario anterior, este error me gustaria que puedas revisar
+        
+        """
+        
+        
+        if request.method == "GET":
+            email_username = session.get('email_username')
+            id_usuario = session.get("user_id")
+        
+            if email_username:
+                print(f"id: {id_usuario}")
+                print(f"Usuario/email: {email_username}")
+                return render_template('inicio.html')
+            else:
+                return redirect('/login')
+        elif request.method == "POST":
+            id_usuario = session.get("user_id")
+            try:
+                nombre_servidor = request.form.get('nombreServidor')
+
+                servidor = Server(nombre=nombre_servidor, created_by=id_usuario)
+                if servidor.create_server():
+                    return redirect(url_for("muro"))
+                else:
+                    return jsonify({'success': False, 'error': 'Error al crear el servidor'})
+
+            except Exception as e:
+                return jsonify({'success': False, 'error': str(e)})
+
+
+
+
+
+        
+        
         
     @app.route("/createuser",methods=["POST"])
     def create():
+        """
+        
+            Recibe la informacion del formulario y la pone dentro de los parametros de la clase User para luego
+            crear el usuario con la funcion create_user
+            (hay un problema que no logro encontrar, cuando creo el usuario tambien guardo el id creado para despues
+            ponerlo en el muro o en la pantalla principal, pero en los print puestos aparece el nombre del usuario anterior
+            y no el usuario recien creado, necesitare que puedas ver eso)
+        
+        """
         try:
             new_username = request.form.get("username")
             new_email = request.form.get("email")
@@ -57,10 +98,10 @@ def init_app():
                             email=new_email,
                             password=new_password)
             
-            new_user.create_user()
+            user_id = new_user.create_user()
+            session["user_id"] = user_id
             return redirect(url_for('muro'))
         except Exception as e:
             return jsonify({"error": str(e)}), 500
     
     return app
-    
